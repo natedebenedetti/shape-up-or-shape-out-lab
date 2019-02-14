492cead4906deb09f4e9fe2f5de9020646db1aaa
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-02-14 10:09:31 -0500

    have styling and layout done in html and css. basic layout in JS

---
+++ script.js
@@ -0,0 +1,41 @@
+class Shape {
+    constructor() {
+
+    }
+
+}
+class Rectangle extends Shape {
+    constructor() {
+        super();
+    }
+}
+class Square extends Shape {
+    constructor() {
+        super();
+    }
+}
+class Circle extends Shape {
+    constructor() {
+        super();
+    }
+}
+class Triangle extends Shape {
+    constructor() {
+        super();
+    }
+}
+
+
+
+
+
+
+
+function openNav() {
+    document.getElementById("mySidePanel").style.width = "250px";
+    
+}
+
+function closeNav() {
+    document.getElementById("mySidePanel").style.width = "0";
+}
