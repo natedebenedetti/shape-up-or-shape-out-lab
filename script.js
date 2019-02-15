4e6e2a1e72cd733a71c201d0146326c3e1253955
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-02-15 13:37:07 -0500

    all but triangle rendering is working. then need to work on randomization of rendering

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 class Shape {
-    constructor() {
-
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.div = $('<div class="shape"></div>');
+        $('#drawing-pad').append(this.div);
+        
     }
-
 }
 class Rectangle extends Shape {
-    constructor() {
-        super();
+    constructor(width, height) {
+        super(width, height);
+        this.div.css({"width": width, "height": height});
     }
 }
 class Square extends Shape {
-    constructor() {
-        super();
+    constructor(sideLength) {
+        super(sideLength, sideLength);
+        this.sideLength = sideLength;
+        this.div.css({"width": sideLength, "height": sideLength});
+
     }
 }
 class Circle extends Shape {
-    constructor() {
-        super();
+    constructor(radius) {
+        super(radius*2, radius*2)
+        this.radius = radius;
+        this.div.css({"width": radius*2, "height": radius*2, "border-radius": "50%"});
+        
+        
     }
 }
 class Triangle extends Shape {
@@ -25,11 +36,27 @@ class Triangle extends Shape {
     }
 }
 
-
-
-
-
-
+$('#make-rec').click(() => {
+    let width = $('#rec-form-w').val();
+    let height = $('#rec-form-h').val();
+    let newRec = new Rectangle(width, height);
+    
+    
+});
+$('#make-sq').click(() => {
+    let sideLength = $('#sq-form').val();
+    let newSq = new Square(sideLength);
+    
+});
+$('#make-cir').click(() => {
+    let radius = $('#cir-form').val();
+    let newCir = new Circle(radius);
+    
+});
+$('#make-tri').click(() => {
+    
+    
+});
 
 function openNav() {
     $('#mySidePanel')[0].style.width = '250px';
