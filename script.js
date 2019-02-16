362003575cc9ae94b1ada6ece78f7114cf6e7529
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-02-16 14:07:58 -0500

    have half of the required details passing into the nav bar

--- script.js
+++ script.js
@@ -1,73 +1,105 @@
 class Shape {
-    constructor(width, height) {
+    constructor(width, height, name) { //parent class "Shape" contains all inherited methods and properties for the child classes.
         this.width = width;
         this.height = height;
+        this.name = name;
         this.div = $('<div class="shape"></div>');
+        this.randomLocation(); // this is working for placing the shapes in random locations, but I need to investigate why they don't stay within the boundaries of the canvas.
         $('#drawing-pad').append(this.div);
-        
+        this.div.hover(() => { // when you mouse over a shape it displays the nav bar with info about that shape, mousing out hides nav bar.
+            openNav();
+            $('#p1').text(`Shape Name: ${name}`);
+            $('#p2').text(`Width: ${width}`);
+            $('#p3').text(`Height: ${height}`);
+        }, () => {
+            closeNav();
+            $('#p1').empty();
+            $('#p2').empty();
+            $('#p3').empty();
+            $('#p4').empty();
+            $('#p5').empty();
+            $('#p6').empty();
+        });
+
+    }
+    randomLocation() { //method that calls on the "randomVals" function below to place the child shapes in absolute postions on the drawing pad.
+        let x = randomVals(0, 600);
+        let y = randomVals(0, 600);
+        this.div[0].style.left = x + "px";
+        this.div[0].style.top = y + "px";
     }
 }
-class Rectangle extends Shape {
-    constructor(width, height) {
-        super(width, height);
-        this.div.css({"width": width, "height": height, "background-color": "green"});
+class Rectangle extends Shape { //child class of shape
+    constructor(width, height, name) {
+        super(width, height, name);
+
+        this.div.css({ "width": width, "height": height, "background-color": "green" });
+
+
     }
 }
-class Square extends Shape {
-    constructor(sideLength) {
-        super(sideLength, sideLength);
+class Square extends Shape { //child class of shape
+    constructor(sideLength, name) {
+        super(sideLength, sideLength, name);
         this.sideLength = sideLength;
-        this.div.css({"width": sideLength, "height": sideLength, "background-color": "red"});
+
+        this.div.css({ "width": sideLength, "height": sideLength, "background-color": "red" });
 
     }
 }
-class Circle extends Shape {
-    constructor(radius) {
-        super(radius*2, radius*2)
+class Circle extends Shape { //child class of shape
+    constructor(radius, name) {
+        super(radius * 2, radius * 2, name);
         this.radius = radius;
-        this.div.css({"width": radius*2, "height": radius*2, "border-radius": "50%", "background-color": "purple"});
-        
-        
-    }
-}
-class Triangle extends Shape {
-    constructor(height) {
-        super(height, height);
-        this.height = height;
-        
-        this.div.css({"width": "0", "height": "0", "border-bottom": "" + height + "px solid yellow", "border-right": "" + height + "px solid transparent"});
-        
-    }
-}
+        this.div.css({ "width": radius * 2, "height": radius * 2, "border-radius": "50%", "background-color": "purple" });
 
+
+    }
+}
+class Triangle extends Shape { //child class of shape
+    constructor(height, name) {
+        super(height, height, name);
+        this.height = height;
+
+        this.div.css({ "width": "0", "height": "0", "border-bottom": "" + height + "px solid yellow", "border-right": "" + height + "px solid transparent" });
+
+    }
+}
+// beginning of button click listeners that create the shapes.
 $('#make-rec').click(() => {
     let width = $('#rec-form-w').val();
     let height = $('#rec-form-h').val();
-    let newRec = new Rectangle(width, height);
-    
-    
+    name = "Rectangle";
+    let newRec = new Rectangle(width, height, name);
 });
 $('#make-sq').click(() => {
     let sideLength = $('#sq-form').val();
-    let newSq = new Square(sideLength);
-    
+    name = "Square";
+    let newSq = new Square(sideLength, name);
 });
 $('#make-cir').click(() => {
     let radius = $('#cir-form').val();
-    let newCir = new Circle(radius);
-    
+    name = "Circle";
+    let newCir = new Circle(radius, name);
 });
 $('#make-tri').click(() => {
     let height = $('#tri-form').val();
-    let newTri = new Triangle(height);
-    
+    name = "Triangle";
+    let newTri = new Triangle(height, name);
 });
+// end of button click listeners for creating shapes.
 
-function openNav() {
-    $('#mySidePanel')[0].style.width = '250px';
-    
+
+function randomVals(min, max) { // function that creates random numbers between 0 and 600
+    return Math.floor(Math.random() * (max - min)) + min;
+
 }
 
-function closeNav() {
+function openNav() { //function that makes the nav bar visible
+    $('#mySidePanel')[0].style.width = '250px';
+
+}
+
+function closeNav() { //function that removes nav bar from visibility.
     $('#mySidePanel')[0].style.width = "0";
 }
